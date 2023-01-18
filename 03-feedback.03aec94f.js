2a8e1868534729012f70bb0150224d844e311a8d
Hlukhenka <112192450+Hlukhenka@users.noreply.github.com>
2023-01-18 15:32:17 +0000

    Deploying to gh-pages from @ Hlukhenka/goit-js-hw-08@a74ed4fa024f03eae43968b18c441c93cc042ccd 🚀

--- 03-feedback.03aec94f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form"),t=(e.querySelector("input"),e.querySelector("textarea"));e.querySelector("button");console.log(t),t.addEventListener("input",(function(e){const t=e.currentTarget.value;console.log(t)}));
-//# sourceMappingURL=03-feedback.03aec94f.js.map
--- 03-feedback.d1eb7959.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),t=(e.querySelector("input"),e.querySelector("textarea"));e.querySelector("button");console.log(t),t.addEventListener("input",(function(e){var t=e.currentTarget.value;console.log(t)}))}();
-//# sourceMappingURL=03-feedback.d1eb7959.js.map
